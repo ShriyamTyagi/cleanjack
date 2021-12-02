f3ce25d8514c3e12f45c9cdf469142a6b3c5873b
Rohit Soner <rsoner@grepruby.io>
2021-12-02 18:12:21 +0530

    right section design

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -6,6 +6,7 @@ import * as AiIcons from "react-icons/ai";
 import { SidebarData } from "./SidebarData";
 import SubMenu from "./SubMenu";
 import { IconContext } from "react-icons/lib";
+import { IoMdLogOut } from "react-icons/io";
 
 const Nav = styled.div`
   background: lightgrey;
@@ -48,7 +49,46 @@ const Sidebar = () => {
   return (
     <>
       <IconContext.Provider value={{ color: "#fff" }}>
-        <Nav></Nav>
+        <section className="d-flex align-items-center justify-content-center">
+          <div>
+            <img src="logo.png" />
+          </div>
+
+          <div className="text-center flex-grow-1 heading">
+            <h1 className="blue">3.3 Beheer Rooters</h1>
+            <p className="blue">
+              3.3.1 Pandlist
+            </p>
+          </div>          
+        </section>
+        <section className="d-flex align-items-center section-border">
+          <div className="flex-grow-1 d-flex align-items-center">
+            <div>
+              <img src="icon.png"/>
+              <span className="red">schoonzorg</span>
+            </div>
+
+            <div className="right-arrow">
+              <span className="">
+                <img src="arrow-left-circle.svg"/>
+              </span>
+            </div>
+          </div>
+
+          <div className="right-dropdown">
+            <div class="dropdown">
+              <button class="dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-expanded="false">
+                <span className="fa fa-user"></span>
+                Rianne Kondon
+              </button>
+              <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                <a class="dropdown-item" href="#">Action</a>
+                <a class="dropdown-item" href="#">Another action</a>
+                <a class="dropdown-item" href="#">Something else here</a>
+              </div>
+            </div>
+          </div>
+        </section>
         <SidebarNav sidebar={sidebar}>
           <SidebarWrap>
             <NavIcon to="#"></NavIcon>
--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,14 +1,14 @@
-import React from "react";
+import React, { useDebugValue } from "react";
 import "./AboutUs.css";
 export const AboutUs = () => {
   return (
-    <div className="home">
+    <div className="home">      
       <div className="info">
         <div className="infoHeader">
-          <p>Mijin Rayon</p>
-          <p>Week 36</p>
+          <p className="user-name">Mijin Rayon</p>
+          <p className="week">Week 36</p>
         </div>
-        <div className="table">
+        {/* <div className="table">
           <div className="column">
             <div className="btns">
               <button>Filter</button>
@@ -60,6 +60,240 @@ export const AboutUs = () => {
             <p>20 uur</p>
             <p>180 uur</p>
             <p>40 uur</p>
+          </div>
+        </div> */}
+
+        <div className="table-resposniev tableFixHead">
+          <table className="">
+            <thead>
+              <tr>
+                <th>
+                  <div className="d-flex">
+                    <button className="filter-button">
+                      Filter
+                    </button>
+                    <input placeholder="Name" />
+                  </div>
+                </th>
+
+                <th>
+                  <div className="d-flex">
+                    <input placeholder="Type" className="type" />
+                  </div>
+                </th>
+
+                <th>
+                  <div className="d-flex">
+                    <input placeholder="Address" className="address" />
+                  </div>
+                </th>
+
+                <th>
+                  <div className="d-flex">
+                    <input placeholder="Contact" />
+                  </div>
+                </th>
+
+                <th>               
+                  <div className="d-flex">
+                    <input placeholder="Name" />
+                  </div>
+                </th>
+
+                <th>
+                  
+                </th>
+
+                <th>
+                  
+                </th>
+              </tr>
+              <tr className="first-row">
+                <td>
+                  <b>pand</b>(subtitle)
+                </td>
+
+                <td className="type">
+                  <b>Type</b>
+                </td>
+
+                <td>
+                  <b>Address</b>
+                </td>
+
+                <td>
+                  <b>Contact</b>
+                </td>
+
+                <td>
+                  <b>Medewerkers</b>
+                </td>
+
+                <td>
+                  <div>
+                  <b>Budget</b><br/>
+                  (contracturen)
+                  </div>
+                </td>
+
+                <td>
+                  <div>
+                  <b>Ingiroostered</b><br/>
+                  (graplande uren Medewerkers)
+                  </div>
+                </td>
+              </tr>
+            </thead>
+
+            <tbody>
+              <tr>
+                <td colSpan="7" className="border-bottom-gray">
+
+                </td>
+              </tr>
+              <tr className="tr-heading">
+                <td colSpan="7">
+                  Pand A
+                </td>
+              </tr>
+
+              <tr>
+                <td>
+                  <div>
+                    <span>
+                      Anton de kom - kleinschalig wonen
+                    </span><br/>
+                    <span>
+                      [service order nummer]
+                    </span>
+                  </div>
+                </td>
+
+                <td className="">
+                  SM#
+                </td>
+
+                <td>
+                Anton de komplein 10/ 3e
+                </td>
+
+                <td>
+                020-3830654 
+                </td>
+
+                <td>
+                  3
+                </td>
+
+                <td>
+                  100 uur
+                </td>
+
+                <td>
+                  100 uur >
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <div>
+                    <span>
+                      Anton de kom - kleinschalig wonen
+                    </span><br/>
+                    <span>
+                      [service order nummer]
+                    </span>
+                  </div>
+                </td>
+
+                <td className="">
+                  SM#
+                </td>
+
+                <td>
+                Anton de komplein 10/ 3e
+                </td>
+
+                <td>
+                020-3830654 
+                </td>
+
+                <td>
+                  3
+                </td>
+
+                <td>
+                  100 uur
+                </td>
+
+                <td>
+                  100 uur >
+                </td>
+              </tr>
+              <tr className="tr-heading">
+                <td colSpan="7">
+                  Pand B
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <div>
+                    <span>
+                    Buitenveldert- kleinschalig wonen
+                    </span><br/>
+                    <span>
+                      [service order nummer]
+                    </span>
+                  </div>
+                </td>
+
+                <td className="">
+                  SM#
+                </td>
+
+                <td>
+                  Doornburg 2
+                </td>
+
+                <td>
+                020-3830654 
+                </td>
+
+                <td>
+                  3
+                </td>
+
+                <td>
+                  100 uur
+                </td>
+
+                <td>
+                  100 uur >
+                </td>
+              </tr>
+            </tbody>
+          </table>
+          <div className="pagination-section d-flex align-items-center justify-content-between">
+            <div>
+              1 tot 25 van 61 resultaten
+            </div>
+
+            <div>
+              <nav aria-label="...">
+                <ul class="pagination">
+                  <li class="page-item disabled">
+                    <a class="page-link" href="#" tabindex="-1" aria-disabled="true">Previous</a>
+                  </li>
+                  <li class="page-item"><a class="page-link" href="#">1</a></li>
+                  <li class="page-item active" aria-current="page">
+                    <a class="page-link" href="#">2</a>
+                  </li>
+                  <li class="page-item"><a class="page-link" href="#">3</a></li>
+                  <li class="page-item">
+                    <a class="page-link" href="#">Next</a>
+                  </li>
+                </ul>
+              </nav>
+            </div>
           </div>
         </div>
       </div>
